705296a3444fa84f2b95425669db21bf34ad65ea
trieu_quan <trieu_quan@fastboy.net>
2022-10-19 15:24:53 +0700

    First commit

---
+++ tests/login.test.ts
@@ -0,0 +1,21 @@
+import test, { chromium } from "@playwright/test";
+import { runInNewContext } from "vm";
+
+test('Login test demo',async()=>{
+ const browser = await chromium.launch();
+ const context = await browser.newContext();
+ const page = await context.newPage();
+ await page.goto('https://ecommerce-playground.lambdatest.io/');
+ await page.hover('//*[@id="widget-navbar-217834"]/ul/li[6]/a/div/span');
+ await page.click('text=Login');
+ await page.waitForTimeout(5000);
+
+
+const newContext= await browser.newContext();
+ const newPage= await newContext.newPage();
+ await newPage.goto('https://google.com/');
+ 
+
+ await page.waitForTimeout(5000)
+
+})
