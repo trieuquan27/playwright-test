e04c3f83234138271475862d34f57a20497b5bec
trieu_quan <trieu_quan@fastboy.net>
2022-10-20 14:47:46 +0700

    updated1

--- tests/alert.test.ts
+++ tests/alert.test.ts
@@ -5,5 +5,6 @@ test ('alert fucntion',async ({page}) => {
     await page.goto("https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo"); 
     await page.click("(//button[contains(@class,'btn btn-dark')])[2]");
 
+    await page.waitForTimeout(5000)
 
 })  
