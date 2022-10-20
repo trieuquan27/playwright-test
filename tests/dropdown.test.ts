42380adada40e469e437858b68fcc3109bc2e1c5
trieu_quan <trieu_quan@fastboy.net>
2022-10-20 17:43:35 +0700

    updated 3

--- tests/dropdown.test.ts
+++ tests/dropdown.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.skip ('dropdown handling',async ({page}) => {
+test ('dropdown handling',async ({page}) => {
  
     await page.goto('https://www.lambdatest.com/selenium-playground/select-dropdown-demo');
     await page.selectOption("id=select-demo",{
@@ -8,28 +8,27 @@ test.skip ('dropdown handling',async ({page}) => {
         // value:"Tuesday",
         index:6,
     });
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(3000);
 
   await page.selectOption('#multi-select',[{
     label:"California",
   },
-   { value:"New York"
-},
-   { index:4,
+//    { value:"New York"
+// },
+//    { index:4,
              
-  }
+//   }
 ]);
     // await page.waitForTimeout(3000);
-    await page.click('#printAll');
-    await page.waitForTimeout(8000);
+
+    await page.locator('#printAll').click({force:true});
+ 
+    await page.waitForTimeout(10000);
 })
 
 
-test("bootstrap dropdown",async({page})=>{
+test.skip("bootstrap dropdown",async({page})=>{
     await page.goto('https://www.lambdatest.com/selenium-playground/jquery-dropdown-search-demo');
-    
-
-
 
 
 })
